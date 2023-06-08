d04ccc6eb7fd728518397cb5159d99bae0571d3e
Tatiana Valeria Nieva <tatiananieva0801@gmail.com>
2023-06-08 13:18:03 -0300

    modificando el espectador

---
+++ Backend/models/espectador.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const EspectadorSchema = new Schema({
+    apellido: { type: String, required: true },
+    nombre: { type: String, required: true },
+    dni: { type: String, required: true },
+    email: { type: String, required: true },
+})
+
+module.exports = mongoose.models.Espectador || mongoose.model('Espectador', EspectadorSchema);
