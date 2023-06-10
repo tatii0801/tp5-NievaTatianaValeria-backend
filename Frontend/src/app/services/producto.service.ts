bc7d53acc62273ef35e0da3bbd7d2685a80aa307
tatii0801 <tatiananieva0801@gmail.com>
2023-06-10 02:06:06 -0300

    modificaciones para el frontend

--- Frontend/src/app/services/producto.service.ts
+++ Frontend/src/app/services/producto.service.ts
@@ -1,9 +1,34 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Producto } from '../models/producto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductoService {
 
-  constructor() { }
+  baseUrl = 'http://localhost:3000/api/producto/';
+  
+  constructor(private http: HttpClient) { }
+
+  public filterByDestacados(): Observable<any> {
+    const httpOptions = {
+      method: 'GET',
+      headers: new HttpHeaders({}),
+      params: new HttpParams({}),
+    };
+    return this.http.get(this.baseUrl, httpOptions);
+  }
+
+  public createProducto(producto: Producto): Observable<any> {
+    const httpOptions = {
+      method: 'POST',
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      }),
+    };
+    let body = JSON.stringify(producto);
+    return this.http.post(this.baseUrl, body, httpOptions);
+  }
 }
